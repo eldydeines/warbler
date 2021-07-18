db0854848bde0c762915d066f683c6a24b7ed688
Eldy Deines <eldy.deines@outlook.com>
2021-07-18 04:10:38 -0600

    completed message testing and tested views

--- test_message_views.py
+++ test_message_views.py
@@ -1,35 +1,18 @@
 """Message View tests."""
 
-# run these tests like:
-#
-#    FLASK_ENV=production python -m unittest test_message_views.py
-
-
 import os
 from unittest import TestCase
 
 from models import db, connect_db, Message, User
 
-# BEFORE we import our app, let's set an environmental variable
-# to use a different database for tests (we need to do this
-# before we import our app, since that will have already
-# connected to the database
-
 os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
-
-
-# Now we can import app
 
 from app import app, CURR_USER_KEY
 
-# Create our tables (we do this here, so we only create the tables
-# once for all tests --- in each test, we'll delete the data
-# and create fresh new clean test data
-
+# Create our tables 
 db.create_all()
 
 # Don't have WTForms use CSRF at all, since it's a pain to test
-
 app.config['WTF_CSRF_ENABLED'] = False
 
 
@@ -44,15 +27,17 @@ class MessageViewTestCase(TestCase):
 
         self.client = app.test_client()
 
-        self.testuser = User.signup(username="testuser",
-                                    email="test@test.com",
-                                    password="testuser",
-                                    image_url=None)
-
+        self.testuser = User(username="testuser", 
+                            email="test@test.com",
+                            password="testuser",
+                            image_url=None)
+        self.testuser_id = self.testuser.id
+        
+        db.session.add(self.testuser)
         db.session.commit()
 
     def test_add_message(self):
-        """Can use add a message?"""
+        """Can user add a message?"""
 
         # Since we need to change the session to mimic logging in,
         # we need to use the changing-session trick:
@@ -71,3 +56,83 @@ class MessageViewTestCase(TestCase):
 
             msg = Message.query.one()
             self.assertEqual(msg.text, "Hello")
+
+    
+    def test_messages_show(self):
+        """Can we see a specific message that has been routed"""
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+            
+            #********** Setup ************#
+            self.testmsg = Message(id=111,text="Test User Message", user_id=self.testuser.id )
+            db.session.add(self.testmsg)
+            db.session.commit()
+
+            #********** Testing ************#
+            resp = c.get(f'/messages/{self.testmsg.id}')
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("Test User Message", html)
+
+    def test_messages_destroy(self):
+        """Check to see if new message is deleted"""
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+            #********** Setup ************#
+            self.testmsg = Message(id=112,text="Test User Message 2", user_id=self.testuser.id )
+            db.session.add(self.testmsg)
+            db.session.commit()
+
+            #********** Testing ************#
+            resp = c.post(f"/messages/{self.testmsg.id}/delete")
+
+            # Make sure it redirects
+            self.assertEqual(resp.status_code, 302)
+
+            msg = Message.query.one_or_none()
+            self.assertIsNone(msg)
+    
+    def test_show_all_messages(self):
+        """Check to see if all messages are shown"""
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+            #********** Setup ************#
+            self.testmsg1 = Message(id=111,text="Test User Message 1", user_id=self.testuser.id )
+            db.session.add(self.testmsg1)
+            self.testmsg2 = Message(id=112,text="Test User Message 2", user_id=self.testuser.id )
+            db.session.add(self.testmsg2)
+            db.session.commit()
+
+            resp = c.get(f'/messages')
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("111", html)
+            self.assertIn("112", html)
+
+    def test_message_like(self):
+        """Check to see likes/unlikes are routed correctly"""
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+           
+           #********** Setup ************#
+            self.testmsg1 = Message(id=111,text="Test User Message 1", user_id=self.testuser.id )
+            db.session.add(self.testmsg1)
+            db.session.commit()
+
+            #********** Testing ************#
+            resp = c.post(f'/messages/111/{self.testuser.id}/like')
+
+            self.assertEqual(resp.status_code, 302)
+            
+            
+
+
